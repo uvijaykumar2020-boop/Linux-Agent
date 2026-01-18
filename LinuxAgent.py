26ddc751639e1fa14f37b8943b169704774cc010
Vijay <u.vijaykumar2020@gmail.com>
2026-01-18 12:24:11 -0500

    model name changed to gemini-2.5-flash.

--- LinuxAgent.py
+++ LinuxAgent.py
@@ -5,7 +5,7 @@ os.environ["GOOGLE_API_KEY"] = ""
 #AI LLM MODEL ACCESS
 from langchain_google_genai import ChatGoogleGenerativeAI
 
-google_llm = ChatGoogleGenerativeAI( model="gemini-1.5-preview", temperature=0.70 )
+google_llm = ChatGoogleGenerativeAI( model="gemini-2.5-flash", temperature=0 )
 
 response = google_llm.invoke(" Tell me how to check the system memory usage on a Linux machine. ")
 

9736ef06d6cee215d85266f41f5df47a843298db
Vijay <u.vijaykumar2020@gmail.com>
2026-01-18 14:32:43 -0500

    Updated

--- LinuxAgent.py
+++ LinuxAgent.py
@@ -7,7 +7,39 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 
 google_llm = ChatGoogleGenerativeAI( model="gemini-2.5-flash", temperature=0 )
 
-response = google_llm.invoke(" How to list file systems on linux machine. ")
+#response = google_llm.invoke(" How to list file systems on linux machine. ")
 
-print(response.content)
+#print(response.content)
 
+from langchain.agents import create_agent
+from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
+
+tools = [shell_tool]
+
+# Agent creation with model and tools passing.
+agent = create_agent(
+        model=google_llm,   
+        tools=tools
+)
+
+UserPrompt = input (" Please enter your requirement")
+
+system_message = SystemMessage(
+                """
+                You are the Linux Administrator and give me last results
+                """
+)
+                
+input_message = HumanMessage(content=UserPrompt)
+
+messages = [
+     system_message,
+	 input_message
+]
+
+# Invoke (replacement for .run
+result = agent.invoke({
+"messages":messages
+})
+
+print(result)

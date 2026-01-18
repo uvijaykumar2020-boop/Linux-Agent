dd3157569c6ba11423d490adc560a1bd55d0846e
Vijay <u.vijaykumar2020@gmail.com>
2026-01-18 13:10:22 -0500

    some changes made.

--- LinuxAgent.py
+++ LinuxAgent.py
@@ -7,7 +7,7 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 
 google_llm = ChatGoogleGenerativeAI( model="gemini-2.5-flash", temperature=0 )
 
-response = google_llm.invoke(" Tell me how to check the system memory usage on a Linux machine. ")
+response = google_llm.invoke(" How to list file systems on linux machine. ")
 
 print(response.content)
 
